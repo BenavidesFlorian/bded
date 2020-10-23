8ffea42cc870fff5cb47d526d36db0b3a5f06840
BenavidesFlorian <37616114+BenavidesFlorian@users.noreply.github.com>
2020-10-23 17:28:13 +0200

    Update Operations.java

--- Operations.java
+++ Operations.java
@@ -1,4 +1,37 @@
 public interface Operations{
-    void Operation();
+    private Compte c;
+    private String type;
+    
+    public Compte getCompte()
+    {
+        return this.Compte;
+    }
+    
+    public String getType()
+    {
+        return this.type;
+    }
+    
+    public Operations()
+    
+    public void Depot(int somme)
+    {
+        System.out.println("Vous avez effectuée un dépot de: " + somme + "€");
+        c.setSolde(c.getSolde()+somme);
+        this.type="Depot";
+    }
+    
+    public void Retrait(int somme)
+    {
+        System.out.println("Vous avez effectuée un débit de: " + somme + "€");
+        c.setSolde(c.getSolde()-somme);
+        this.type="Retrait";
+    }
+    
+    public void Consultation()
+    {
+        this.type="Consultation";
+        System.out.println("Votre solde est de: "+c.getSolde() + "€");
+    }
 }
 

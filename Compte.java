b7604263b8fa5c7c61c58d1b4dbfcf6e98191791
beltramem <32321438+beltramem@users.noreply.github.com>
2020-11-04 11:13:07 +0100

    Delete Compte.java

--- Compte.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.*;
-
-public interface Compte extends Remote {
-    public String Depot(int somme) throws java.rmi.RemoteException;
-    public String Retrait(int somme) throws java.rmi.RemoteException;
-    public String Consultation() throws java.rmi.RemoteException;
-    public String ConsultationOp() throws java.rmi.RemoteException;
-    public int getSolde() throws java.rmi.RemoteException;
-} 
